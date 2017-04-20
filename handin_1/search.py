fe69a1ff4e5815adac93b8a2294d9b137b446a65
Jacob Bieker <jacob@bieker.tech>
2017-04-20 19:14:41 +0200

    Add timing construction

--- handin_1/search.py
+++ handin_1/search.py
@@ -5,7 +5,7 @@ Using suffix tree for searching in filename
 
 """
 from McM_suffix_tree_build import built_tree
-import os, sys
+import os, sys, time
 
 if __name__ == "__main__":
     filename = sys.argv[0]
@@ -14,6 +14,11 @@ if __name__ == "__main__":
     with open(filename, "r") as datafile:
         data = datafile.read().replace('\n', '')
         # Build tree from data file
+        start = time.time()
         tree = built_tree(data, False)
+        end = time.time()
+        print("Construction Time")
+        print(end - start)
         print("Size per character (On average): ")
         print(sys.getsizeof(data)/len(data))
+        
