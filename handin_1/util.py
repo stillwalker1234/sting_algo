03a4ca70b55d8371d98376aaf3e414e896bd9de2
Jonas Tranberg <mail@stillwalker.dk>
2017-04-20 20:57:32 +0200

    added search

--- handin_1/util.py
+++ handin_1/util.py
@@ -1,11 +1,7 @@
 def print_tree(node, _str):
     def _print_sub(_node, depth):
-        print(('_' * depth) + str(_node[2]) + " : " + _str[_node[2][0]:_node[2][1]])
-        if _node[2][0] > _node[2][1]:
-            raise Exception()
-            print("!")
-            exit(0)
-            
+        print(('_' * depth) + str(_node[2]) + " : " + _str[_node[2][0]:_node[2][1]] + " : " + (str(_node[3]) if not type(_node[3]) is list else ""))
+        
         for n in _node[0]:
             _print_sub(n, depth+1)
 
