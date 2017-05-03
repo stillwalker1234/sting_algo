2949d3932d49b77eda95b40015cfcfda81c89975
Jacob Bieker <jacob@bieker.tech>
2017-05-03 09:26:29 +0200

    Rewrite tandem repeat finder
    Now uses the new setup of the data stored at the nodes to go through the leaf-list and dfs array. Still need to iterate through the nodes though

--- handin_2/search.py
+++ handin_2/search.py
@@ -40,54 +40,49 @@ def search(tree, query, string):
 
 
 def find_tandem_repeats(tree, string):
-    # TODO Add in DFS array
     # Takes a node in a tree, and get the branching tandem repeats
     def _get_largest_subnode(node):
         # Get v' and its leaf-list to subtract from the leaf-list of v Step 2a
-        largest_subnode = None
-        largest_subleaflist = (0,0)
+        largest_subleaflist = []
         for subnode in node[0]:
-            if subnode[2][1] - subnode[2][0] > largest_subleaflist[1] - largest_subleaflist[2]:
+            if len(subnode[-1]) > len(largest_subleaflist):
                 # Larger than the current range, so the largest one
-                largest_subleaflist = subnode[2]
-                largest_subnode = subnode
-        # Now remove the largest sub-leaf-list from the node v's leaf-list, i.e. the children of v and not v'
-        leaf_list_prime_v = node[0] - largest_subnode[0]
-        leaf_list_prime_v = []
-        # Now have list of child nodes not in the largest subnode, not leaf-list though. Leaf list would just be range?
-        new_range_start = (node[2][0], largest_subnode[2][0] - node[2][0])
-        new_range_end = (node[2][1] - largest_subnode[2][1], node[2][1])
-        # new_range_start, new_range_end are the ranges of leafs not under LL(v')
-        for i in range(new_range_start[0], new_range_start[1]):
-            leaf_list_prime_v.append(i)
-        for i in range(new_range_end[0], new_range_end[0]):
-            leaf_list_prime_v.append(i)
+                largest_subleaflist = subnode[-1]
+        # Now remove the largest sub-leaf-list from the node v's leaf-list, i.e. the leaves of v and not v'
+        leaf_list_prime_v = [leaf for leaf in node[-1] if leaf not in largest_subleaflist]
         return leaf_list_prime_v
 
-    def _step_2(leaf_list_prime, leaf_list, depth, foreward):
+    def _step_2(leaf_list_prime, leaf_list, depth, dfs, v, foreward):
         """
         
         :param leaf_list_prime: the list of leafs in LL'(v)
         :param leaf_list: the list of leafs in LL(v)
         :param depth: Depth of node v
+        :param dfs: DFS array, indexed by leaf-list number
+        :param v: Node v, the root of the subtree we are looking at
         :param foreward: whether to do foreward or backwards search, boolean
         :return: list of tandem repeats and their lengths
         """
         # Get the leaf number for every leaf in the list, that number + depth gives whether its in the big leaf-list
         tandem_repeats = []
         # Steps 2b and 2c: For each leaf i in LL'(v), test whether leaf j = i +- D(v) is in LL(v)
+        # To do that, just check whether, for leaf j, DFS[j] is between the DFS values recorded at v
         for leaf in leaf_list_prime:
             if foreward:
-                if leaf + depth in leaf_list:
+                # Checking if DFS[j] is between the ranges of DFS numbers stored at v
+                if v[-2][1] > dfs[leaf + depth] > v[-2][0]:
                     # First check is true
                     if string[leaf] != string[leaf + 2*depth]:
                         # Check whether S[i] != S[i + 2D(v)]
+                        # Second check is true, so tandem repeat
                         tandem_repeats.append([leaf, 2*depth])
             else:
-                if leaf - depth in leaf_list:
+                # Checking if DFS[j] is between the ranges of DFS numbers stored at v
+                if v[-2][1] > dfs[leaf + depth] > v[-2][0]:
                     # First check is true
                     if string[leaf] != string[leaf + 2*depth]:
                         # Check whether S[i] != S[i + 2D(v)]
+                        # Second check is true, so tandem repeat
                         tandem_repeats.append([leaf, 2*depth])
         return tandem_repeats
 
